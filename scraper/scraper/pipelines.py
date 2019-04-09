df796572679d513b4f9117fb3161c5fdc4d7054f
Akash Nigam <akashnigam2012@gmail.com>
2019-04-09 02:56:39 -0700

    removed comments and redundant code

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -1,4 +1,3 @@
-import json
 import csv
 from datetime import datetime
 
@@ -17,7 +16,6 @@ class ScraperPipeline(object):
         csvOutputPath = "data/" + datetime.today().strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
         self.outputFile = open(csvOutputPath, "a")
         row = ['disease', 'postLink', 'postHeading', 'postContent']
-        print(row)
         writer = csv.writer(self.outputFile)
         writer.writerow(row)
 
@@ -25,54 +23,11 @@ class ScraperPipeline(object):
         self.outputFile.close()
 
     def process_item(self, item, spider):
-        print('--------------------------------------')
-        print('--------------------------------------')
-        print('--------Inside Pipeline---------------')
-        print(item)
-        print('--------------------------------------')
         if item['contentType'] == 'disease':
             self.linkDisease[item['postLink']] = item['disease']
-            print(json.dumps(self.linkDisease))
         else:
             disease = self.linkDisease[item['postLink']]
             row = [disease, item['postLink'], item['postHeading'], item['postContent']]
-            print(row)
             writer = csv.writer(self.outputFile)
             writer.writerow(row)
-        print('--------------------------------------')
-        print('--------------------------------------')
-        #print("##########################################")
-        #print(item)
-        #print("##########################################")
-        return item
-
-class MysqlPipeline(object):
-    #collection_name = 'scrapy_items'
-
-    '''
-    def __init__(self, mongo_uri, mongo_db):
-        self.mongo_uri = mongo_uri
-        self.mongo_db = mongo_db
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        return cls(
-            mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
-        )
-
-
-    def open_spider(self, spider):
-        self.client = pymongo.MongoClient(self.mongo_uri)
-        self.db = self.client[self.mongo_db]
-
-    def close_spider(self, spider):
-        self.client.close()
-
-    '''
-
-    def process_item(self, item, spider):
-        print("##########################################")
-        print(item)
-        print("##########################################")
         return item
--- scraper/scraper/spiders/patient_info_spider.py
+++ scraper/scraper/spiders/patient_info_spider.py
@@ -1,53 +1,33 @@
 import scrapy
-import pandas as pd
 
 class PatientInfoSpider(scrapy.Spider):
 
     name = "patient_info"
 
-    mylist = []
-
     def start_requests(self):
         urlPrefix = 'https://patient.info/forums/index-'
         alphabet = 'A'
         i = 0
-        #print("##########################################")
         while i<1:
             url = urlPrefix + chr(ord(alphabet) + i)
             yield scrapy.Request(url=url, callback=self.parse)
             i += 1
-        #print(self.mylist)
-        #print("##########################################")
 
     def parse(self, response):
-        #print("##########################################")
         aToZgroups = response.xpath("//div[@class='disc-forums disc-a-z']")
         symtomRowsLinks = aToZgroups.xpath(".//tr[@class='row-0']//a/@href")
-        symtomRowsLinks = symtomRowsLinks[:20]
-        symtomCompleteLink = {}
+        symtomRowsLinks = symtomRowsLinks[:]
         for symptomLink in symtomRowsLinks:
-            print(symptomLink)
-            #symtomCompleteLink['hjh'] = response.urljoin(symptomLink.get())
             symtomCompleteLink = response.urljoin(symptomLink.get())
-            #print(symtomCompleteLink)
-            #break
-            print(response.url)
-            print(symtomCompleteLink)
-            #yield symtomCompleteLink
             yield scrapy.Request(url=symtomCompleteLink, callback=self.parsePostsList)
-            #exit()
-
-        #print("##########################################")
 
     def parsePostsList(self, response):
-        #print("##########################################")
         disease = response.xpath("//h1[@class='articleHeader__title masthead__title']/text()").get().strip()
         posts = response.xpath("//li[@class='disc-smr cardb']")
-        posts = posts[0:20]
+        posts = posts[:]
         for postHeading in posts:
             postLinkAnchorTag = postHeading.xpath(".//h3[@class='post__title']//a/@href")
             postLink = response.urljoin(postLinkAnchorTag.get())
-            print(postLink)
             mydict = {
                 'contentType': 'disease',
                 'disease': disease,
@@ -55,18 +35,14 @@ class PatientInfoSpider(scrapy.Spider):
             }
             yield mydict
             yield scrapy.Request(url=postLink, callback=self.parsePost)
-            #exit()
 
     def parsePost(self, response):
-        #print("##########################################")
         postLink = response.url
         postHeading = response.xpath("//h1[@class='u-h1 post__title']/text()").get()
         postContentParas = response.xpath("//div[@class='post__content']/p/text()")
         postContent = ''
         for para in postContentParas:
             postContent += para.get() + ' '
-        self.mylist.append([postHeading, postContent])
-        #item = {'postcontent':[postHeading, postContent]}
         item = {
             'contentType': 'userPost',
             'postLink': postLink,
@@ -74,15 +50,3 @@ class PatientInfoSpider(scrapy.Spider):
             'postContent': postContent
         }
         yield item
-        #yield item
-        #print("##########################################")
-
-    def closed(self, reason):
-        #print("##########################################")
-        #print("##########################################")
-        df = pd.DataFrame(self.mylist, columns=['heading','passage'], index=None)
-        print(df.shape)
-        print(df.head())
-        df.to_csv('patientInfo.csv')
-        #print("##########################################")
-        #print("##########################################")
