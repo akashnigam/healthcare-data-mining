e6a49e092dd5b0d41cbb02abc1d21690eb73b237
Shubham Gupta <shubhamgupta372@gmail.com>
2019-04-21 12:40:29 -0700

    added treatment insertion

--- ontology/complete_ontology_insertion_prc.sql
+++ ontology/complete_ontology_insertion_prc.sql
@@ -1,7 +1,12 @@
+USE `ontology`;
+DROP procedure IF EXISTS `ontology_insertion_prc`;
+
+DELIMITER $$
+USE `ontology`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `ontology_insertion_prc`()
 BEGIN
   DECLARE done INT DEFAULT FALSE ;
-  DECLARE d_id,d_name,s_id,s_name TEXT;
+  DECLARE d_id,d_name,s_id,s_name,t_id,t_name,dr_id,dr_name,b_id,b_name TEXT;
   DECLARE cur1 CURSOR FOR SELECT Diseaseid,DiseaseName,SymptomId,SymptomName,TreatmentId,TreatmentName,DrugId,DrugName,BodypartId,BodypartName
   FROM ontology.patientsinfo p1;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -52,16 +57,16 @@ BEGIN
 	
 	IF dr_id<>"" THEN
 		UPDATE ontology.diseases SET has_treatment_count=has_treatment_count+1 WHERE disease_id=d_id;
-		SELECT count(1) INTO @td_cnt FROM ontology.treatment WHERE treatment_id=t_id;
+		SELECT count(1) INTO @td_cnt FROM ontology.treatment WHERE treatment_id=dr_id;
         IF @td_cnt=0 THEN
 			INSERT INTO ontology.treatment VALUES (t_id,t_name,"Drug");
 		END IF;
-        SELECT count(1) INTO @dis_tr1_cnt FROM ontology.disease_treatments WHERE disease_id=d_id and treatment_id=t_id;
+        SELECT count(1) INTO @dis_tr1_cnt FROM ontology.disease_treatments WHERE disease_id=d_id and treatment_id=dr_id;
         IF @dis_tr1_cnt=0 THEN
-			INSERT INTO ontology.disease_treatments VALUES (d_id,t_id,0);
+			INSERT INTO ontology.disease_treatments VALUES (d_id,dr_id,0);
 		ELSE
 			UPDATE ontology.disease_treatments SET count=count+1
-            WHERE treatment_id=t_id AND disease_id=d_id;
+            WHERE treatment_id=dr_id AND disease_id=d_id;
 		END IF;
     END IF;
 	
