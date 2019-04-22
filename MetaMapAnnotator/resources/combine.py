18d29bd61da513762bce75f2f6402e8734fec1f0
Akash Nigam <akashnigam2012@gmail.com>
2019-04-21 22:29:16 -0700

    adding script to merge metamap output csv files

---
+++ MetaMapAnnotator/resources/combine.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+files = [f for f in os.listdir('.') if os.path.isfile(f) and ("Drugs" in f or "PatientsInfo" in f or "Webmd" in f)]
+
+prevFilesTotalPosts = 0
+allRows = []
+headers = []
+for file in files:
+    print(file)
+    with open(file) as f:
+        reader = csv.reader(f)
+        headers = next(reader, None)
+        data = [r for r in reader]
+        lastPostNumber = 0
+        for index, row in enumerate(data):
+            postNumber = int(row[0])
+            row[0] = prevFilesTotalPosts + postNumber
+            allRows.append(row)
+            lastPostNumber = postNumber
+        prevFilesTotalPosts += lastPostNumber
+
+with open('cobinedData.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerow(headers)
+    writer.writerows(allRows)
+    writeFile.close()
+
+print("Comnined CSV file successfully created")
