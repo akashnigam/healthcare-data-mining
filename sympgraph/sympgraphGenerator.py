eb5b398a032126c636ad5cfdb0423b61ef6b0d36
Akash Nigam <akashnigam2012@gmail.com>
2019-04-22 20:18:01 -0700

    adding sample sympgraph with 5 posts

--- sympgraph/sympgraphGenerator.py
+++ sympgraph/sympgraphGenerator.py
@@ -1,8 +1,9 @@
 import csv
 import numpy as np
-import json
+import matplotlib.pyplot as plt
+import networkx as nx
 
-def getPostWiseSymptoms():
+def getPostWiseSymptoms(numPosts):
     file = '../MetaMapAnnotator/resources/combinedData.csv'
     allpostSymptoms = {}
     allSymptoms = {}
@@ -12,19 +13,24 @@ def getPostWiseSymptoms():
     with open(file) as f:
         reader = csv.reader(f)
         headers = next(reader, None)
-        for row in reader:
+        data = [r for r in reader]
+        #data = data[0:numPosts]
+        for row in data:
             postId = row[0]
             symptomId = row[3]
             symptomName = row[4]
             if symptomId:
                 if postId not in allpostSymptoms:
-                    allpostSymptoms[postId] = {"index": postIndex, "symptomList": []}
+                    allpostSymptoms[postId] = {"index": postIndex, "symptomList": [], "symtomNames": []}
                     postIndex += 1
                 allpostSymptoms[postId]["symptomList"].append(symptomId)
+                allpostSymptoms[postId]["symtomNames"].append(symptomName)
                 if symptomId not in allSymptoms:
                     allSymptoms[symptomId] = {"index": symtomIndex, "name": symptomName}
                     symtomsReverseMap[symtomIndex] = symptomId
                     symtomIndex += 1
+                if numPosts != None and postIndex> numPosts:
+                    break
     return allpostSymptoms, allSymptoms, symtomsReverseMap
 
 def postWiseSymptomMatrix(allpostSymptoms, allSymptoms):
@@ -39,28 +45,16 @@ def postWiseSymptomMatrix(allpostSymptoms, allSymptoms):
     return postSymptomsMat
 
 def createSympGraph(postSymptomsMat, allSymptoms):
-    #print(len(allSymptoms))
     postSymptomsMat = np.array(postSymptomsMat)
     symptomGraphShape = (len(allSymptoms),len(allSymptoms))
     symptomGraph = np.zeros(symptomGraphShape)
-    #print("symptomGraphShape:", symptomGraph.shape)
 
     for postSymptoms in postSymptomsMat:
         postSymptoms = np.matrix(postSymptoms)
         postSymptomGraph = np.matmul(postSymptoms.transpose(), postSymptoms)
         symptomGraph += postSymptomGraph
-        #print("shape:", postSymptoms.transpose().shape)
-        #print("shape:", postSymptoms.shape)
-        #print("shape:", postSymptomGraph.shape)
-        #print("symptomGraphShape:", symptomGraph.shape)
-        #print(postSymptomGraph)
-        #exit()
 
-    #print(postSymptomsMat.shape)
-    #print(symptomGraph.shape)
     print(symptomGraph.shape)
-    #print(json.dumps(allSymptoms))
-    #print(json.dumps(postSymptomsMat))
     return symptomGraph
 
 def getSymgraphEdges(symptomGraph, symtomsReverseMap):
@@ -75,6 +69,21 @@ def getSymgraphEdges(symptomGraph, symtomsReverseMap):
                 edgeList.append([source, dest, weight])
     return edgeList
 
+def plotGraph(edgeList, allSymptoms):
+    G = nx.Graph()
+    for edge in edgeList:
+        G.add_edge(allSymptoms[edge[0]]["name"],allSymptoms[edge[1]]["name"])
+    print("number of nodes:", G.number_of_nodes())
+    print("number of edges:", G.number_of_edges())
+    options = {
+        'node_color': 'black',
+        'node_size': 5,
+        'width': 1,
+        'with_labels': True
+    }
+    nx.draw_circular(G, **options)
+    plt.show()
+
 def writeSympGraph(edgeList):
     with open('sympgraph.csv', 'w') as writeFile:
         writer = csv.writer(writeFile)
@@ -83,10 +92,17 @@ def writeSympGraph(edgeList):
         writeFile.close()
 
 if __name__ == '__main__':
-    allpostSymptoms, allSymptoms, symtomsReverseMap = getPostWiseSymptoms()
+    allpostSymptoms, allSymptoms, symtomsReverseMap = getPostWiseSymptoms(None)
     postSymptomsMat = postWiseSymptomMatrix(allpostSymptoms, allSymptoms)
     symptomGraph = createSympGraph(postSymptomsMat, allSymptoms)
     edgeList = getSymgraphEdges(symptomGraph, symtomsReverseMap)
     writeSympGraph(edgeList)
-    print("Successully generated Sympgraph")
+    print("Successully generated Sympgraph and saved in CSV")
+
+    allpostSymptoms, allSymptoms, symtomsReverseMap = getPostWiseSymptoms(5)
+    postSymptomsMat = postWiseSymptomMatrix(allpostSymptoms, allSymptoms)
+    symptomGraph = createSympGraph(postSymptomsMat, allSymptoms)
+    edgeList = getSymgraphEdges(symptomGraph, symtomsReverseMap)
+    plotGraph(edgeList, allSymptoms)
+    print("Successully generated Sympgraph with 5 posts and plotted sympgraph")
 
